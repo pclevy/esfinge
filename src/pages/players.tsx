674af08ee46f436b972db0b4ab2b431c41d6574a
pclevy <pclevybr@gmail.com>
2026-08-07 02:24:43 -0300

    '/players' - 2026/08/07, 02:24

--- src/pages/players.tsx
+++ src/pages/players.tsx
@@ -2,84 +2,90 @@ import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 
 interface Player {
-    reg: string;
-    nome: string;
-    clube: string;
-    municipio: string;
-    rating: string;
+  reg: string;
+  nome: string;
+  clube: string;
+  municipio: string;
+  rating: string;
 }
 
 function Players() {
-    const [players, setPlayers] = useState<Player[]>([]);
-    const [query, setQuery] = useState('');
-    const timeoutRef = useRef<number | null>(null);
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [query, setQuery] = useState("");
+  const timeoutRef = useRef<number | null>(null);
 
-    const httpClient = axios.create({
-        baseURL: 'http://localhost:8000',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+  const httpClient = axios.create({
+    baseURL: "http://localhost:8000",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    function fetchPlayers() {
-        httpClient.get('/')
-            .then(response => {
-                setPlayers(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching players:', error);
-            });
+  function fetchPlayers() {
+    httpClient
+      .get("/")
+      .then((response) => {
+        setPlayers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching players:", error);
+      });
+  }
+
+  function findPlayer(search: string) {
+    httpClient
+      .post("/", {
+        query: search,
+      })
+      .then((response) => {
+        setPlayers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching players:", error);
+      });
+  }
+
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
+
+  useEffect(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
     }
 
-    function findPlayer(search: string) {
-        httpClient.post('/', {
-            query: search
-        })
-        .then(response => {
-            setPlayers(response.data);
-        })
-        .catch(error => {
-            console.error('Error fetching players:', error);
-        });
+    if (query) {
+      timeoutRef.current = window.setTimeout(() => {
+        findPlayer(query);
+      }, 2000);
+    } else {
+      fetchPlayers();
     }
 
-    useEffect(() => {
-        fetchPlayers();
-    }, []);
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [query]);
 
-    useEffect(() => {
-        if (timeoutRef.current) {
-            clearTimeout(timeoutRef.current);
-        }
+  return (
+    <div style={{ textAlign: "left" }}>
+      <h1>Jogadores</h1>
 
-        if (query) {
-            timeoutRef.current = window.setTimeout(() => {
-                findPlayer(query);
-            }, 2000);
-        } else {
-            fetchPlayers();
-        }
+      <input
+        type="text"
+        placeholder="Buscar jogador..."
+        onChange={(e) => setQuery(e.target.value)}
+      />
 
-        return () => {
-            if (timeoutRef.current) {
-                clearTimeout(timeoutRef.current);
-            }
-        };
-    }, [query]);
-
-    return (
-        <div>
-            <h1>Jogadores</h1>
-
-            <input type="text" placeholder="Buscar jogador..." onChange={(e) => setQuery(e.target.value)} />
-
-            <ul>
-                {players.map(player => {
-                    return <li>{player.nome}</li>;
-                })}
-            </ul>
-        </div>
-    )
+      <ul>
+        {players.map((player) => {
+          return <li>{player.nome}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Players;
